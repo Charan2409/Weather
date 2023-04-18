989123d20c25eb6d31f9f223d8909a3620d69504
saicharanbandi <saicharanbandi1208@gmail.com>
2023-04-19 03:13:24 +0530

    Commit initial code into github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
+import Image from "./Image.jpg";
 
-function App() {
+function App({ data }) {
+  const [city, setCity] = useState("");
+  const [result, setResult] = useState("");
+
+  const changeHandler = (e) => {
+    setCity(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1fe4c65616186b87571934f0ae487d6e`
+    )
+      .then((response) => response.json())
+      .then(data => {
+        console.log(data);
+        const kelvin = data.main.temp;
+        const desc = data.weather[0].description;
+        console.log(desc);
+        const celsius = kelvin - 273.15;
+        setResult(Math.round(celsius));
+      }).catch(error => console.log(error))
+      setCity("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: "url(/Image.jpeg)" }}>
+      <center>
+        <h1>Weather App</h1>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            name="City"
+            placeholder="please enter city name"
+            onChange={changeHandler}
+          />
+          <input type="submit" value="Get Temperature" />
+        </form>
+
+        <h4>Temperature : {result} °C </h4>
+      </center>
     </div>
   );
 }
