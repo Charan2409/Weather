d80407da4809a93b12e9fa82d9d275fbe3ceba13
saicharanbandi <saicharanbandi1208@gmail.com>
2023-04-20 00:44:27 +0530

    updating code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import "./index.css";
 import Image from "./Image.jpg";
 
-function App({ data }) {
+function App() {
   const [city, setCity] = useState("");
   const [result, setResult] = useState("");
+  const [data, setData] = useState({});
 
   const changeHandler = (e) => {
     setCity(e.target.value);
@@ -17,19 +18,19 @@ function App({ data }) {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1fe4c65616186b87571934f0ae487d6e`
     )
       .then((response) => response.json())
-      .then(data => {
+      .then((data) => {
         console.log(data);
+        setData(data);
         const kelvin = data.main.temp;
-        const desc = data.weather[0].description;
-        console.log(desc);
         const celsius = kelvin - 273.15;
         setResult(Math.round(celsius));
-      }).catch(error => console.log(error))
-      setCity("");
+      })
+      .catch((error) => console.log(error));
+    setCity("");
   };
 
   return (
-    <div className="App" style={{ backgroundImage: "url(/Image.jpeg)" }}>
+    <div className="App" style={{ backgroundImage: `url(${Image})` }}>
       <center>
         <h1>Weather App</h1>
         <form onSubmit={submitHandler}>
@@ -41,8 +42,10 @@ function App({ data }) {
           />
           <input type="submit" value="Get Temperature" />
         </form>
-
-        <h4>Temperature : {result} °C </h4>
+        <br></br>
+        <h3>Temperature : {result} °C </h3>
+        <h3>Weather description : {data?.weather?.[0]?.description}</h3>
+        <h3>WindSpeed: {data?.wind?.speed}</h3>
       </center>
     </div>
   );
